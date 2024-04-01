7edb4f2924d2ff8200fbedcf46e2ae15524beea6
Will Metzler <metzlerwill@gmail.com>
2024-04-01 10:58:44 -0400

    pass tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react";
+
+function Header(props) {
+  return <h1>hello from the Header!</h1>;
+}
+
+export default Header;
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
